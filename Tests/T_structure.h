a7a27c137447d92251a7b20a2ed22f6124aaddf2
Andrea Casalino <andrecasa91@gmail.com>
2022-02-04 20:01:07 +0100

    testing

--- Tests/T_structure.h
+++ Tests/T_structure.h
@@ -12,6 +12,10 @@ using namespace xmlPrs;
 ///         D
 ///         |
 ///         E
+///
+/// tag A has attributes {"A1 = a1", "A2 = a2"}
+/// tag B has attributes {"B1 = b1"}
+/// tag E has attributes {"E1 = e1"}
 Root generate_T_structure();
 
 bool is_valid_T_structure(const Root &root);
