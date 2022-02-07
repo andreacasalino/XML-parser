5033fe6380cc4235d1e3dbceaf848faeb80356c4
Andrea <andrecasa91@gmail.com>
2022-02-07 23:10:49 +0000

    implementing

---
+++ Lib/include/XML-Parser/Converter.h
@@ -0,0 +1,19 @@
+/**
+ * Author:    Andrea Casalino
+ * Created:   03.12.2019
+ *
+ * report any bug to andrecasa91@gmail.com.
+ **/
+
+#ifdef ENABLE_XML_Parser_nlohmann_json_converter
+#pragma once
+
+#include <XML-Parser/Tag.h>
+#include <nlohmann/json.hpp>
+
+namespace xmlPrs {
+    /// TODO explain that multimap may have same nested tag name, which is however invalid for json
+    nlohmann::json to_json(const Root& xml_structure);
+}
+
+#endif
---
+++ Lib/source/Converter.cpp
@@ -0,0 +1,20 @@
+#ifdef ENABLE_XML_Parser_nlohmann_json_converter
+#include <XML-Parser/Converter.h>
+
+namespace xmlPrs {
+    namespace {
+        void add(nlohmann::json& recipient, const Tag& to_add) {
+            for (const auto& nested : ) {
+
+            }
+        }
+    }
+
+    nlohmann::json to_json(const Root& xml_structure) {
+        nlohmann::json result;
+
+        return result;
+    }
+}
+
+#endif
